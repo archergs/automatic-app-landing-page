88ddf3a35e22c94e82f9c9dc840e7c590a76c3ac
Archer Gardiner-Sheridan <30485025+archergs@users.noreply.github.com>
2022-03-30 19:01:15 +1030

    Create urbanwatch_wearos.mk

---
+++ policies/urbanwatch_wearos.mk
@@ -0,0 +1,5 @@
+## UrbanWatch for Wear OS Privacy Policy
+
+You'll be glad to know UrbanWatch tracks you in no way whatsoever! However, it does reach out to api.urbandictionary.com, which may log ur IP Address 
+among other information. The usage of your data by the site is subject to the Urban Dictionary Privacy Policy, which can be found here: 
+(https://about.urbandictionary.com/privacy)[https://about.urbandictionary.com/privacy]
